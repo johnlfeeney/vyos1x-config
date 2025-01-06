19c8abe3e755af402c008415567a2ba3ac470088
John Estabrook <jestabro@vyos.io>
2025-01-06 12:08:10 -0600

    T7046: add merge_reference_tree_cache and cache i/o
    To support addon packages with separate interface-definitions dirs,
    refactor reference_tree_to_json.

--- src/generate.mli
+++ src/generate.mli
@@ -3,3 +3,6 @@ exception Write_error of string
 
 val load_interface_definitions : string -> (Reference_tree.t, string) result
 val reference_tree_to_json : ?internal_cache:string -> string -> string -> unit
+val interface_definitions_to_cache : string -> string -> unit
+val reference_tree_cache_to_json : string -> string -> unit
+val merge_reference_tree_cache : string -> string -> string -> unit
